b59812fc77240bffd0b500e88cfa9378b65d33a6
Rob Frawley 2nd <rmf@src.run>
2017-06-21 01:33:29 -0400

    basic html structure, use bootstrap-sass@4, cleanup webpack config

--- client/js/case.js
+++ client/js/case.js
@@ -1,4 +1,13 @@
 
+/*
+ * This file is part of the `src-run/rf-app` project.
+ *
+ * (c) Rob Frawley 2nd <rmf@src.run>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 class Case {
     constructor(selector) {
         this.elements = $(selector);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
 
-const path = require('path');
+/*
+ * This file is part of the `src-run/rf-app` project.
+ *
+ * (c) Rob Frawley 2nd <rmf@src.run>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 const webpack = require('webpack');
 
 const plugins = {
@@ -7,17 +15,18 @@ const plugins = {
     extractText: require('extract-text-webpack-plugin'),
     manifest:    require('webpack-manifest-plugin'),
     md5:         require('webpack-md5-hash'),
+    path:        require('path'),
 };
 
 const origins = {
-    context: path.resolve(__dirname, './client'),
-    entries: { app: './js/app.js' }
+    context: plugins.path.resolve(__dirname, './client'),
+    entries: { app: './js/app.js' },
 };
 
 const outputs = {
+    fileNameTpl: process.env.NODE_ENV === 'prod' ? '[name].[chunkhash]' : '[name]',
     outsidePath: '/assets/',
-    privatePath: path.resolve(__dirname, `./web/assets`),
-    fileNameTpl: process.env.NODE_ENV === 'prod' ? '[name].[chunkhash]' : '[name]'
+    privatePath: plugins.path.resolve(__dirname, `./web/assets`),
 };
 
 const config = {
@@ -27,26 +36,24 @@ const config = {
     output: {
         path:       outputs.privatePath,
         publicPath: outputs.outsidePath,
-        filename:   `${outputs.fileNameTpl}.js`
+        filename:   `${outputs.fileNameTpl}.js`,
     },
     module: {
         rules: [
             { test: /\.js$/i, exclude: [/node_modules/], use: 'babel-loader' },
             { test: /\.scss$/i, use: plugins.extractText.extract({ fallback: 'style-loader', use: [
                 { loader: 'css-loader', options: { sourceMap: true } },
-                { loader: 'postcss-loader', options: { sourceMap: true,
-                    plugins: (loader) => [
-                        require('postcss-import')({ root: loader.resourcePath }),
-                        require('autoprefixer')(),
-                        require('cssnano')(),
-                        require('postcss-browser-reporter')(),
-                        require('postcss-reporter')(),
-                    ]
-                } },
-                { loader: 'sass-loader', options: {  sourceMap: true  } }
+                { loader: 'postcss-loader', options: { sourceMap: true, plugins: (loader) => [
+                    require('postcss-import')({ root: loader.resourcePath }),
+                    require('autoprefixer')(),
+                    require('cssnano')(),
+                    require('postcss-browser-reporter')(),
+                    require('postcss-reporter')(),
+                ] } },
+                { loader: 'sass-loader', options: {  sourceMap: true  } },
             ]})},
             { test: /\.jpg$/i, use: 'file-loader' },
-            { test: /\.png$/i, use: { loader: 'url-loader', options: { limit: 10000 } } }
+            { test: /\.png$/i, use: { loader: 'url-loader', options: { limit: 10000 } } },
         ],
     },
     plugins: [
@@ -58,12 +65,13 @@ const config = {
         new plugins.manifest({ publicPath: outputs.outsidePath }),
         new plugins.extractText({ filename: `${outputs.fileNameTpl}.css` }),
         new plugins.cleanup(),
-    ]
+    ],
 };
 
-if (process.env.NODE_ENV === 'prod') {
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: true } }));
-    config.plugins.push(new plugins.md5())
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
+    config.plugins.push(new plugins.md5());
+    //config.plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }));
 }
 
 module.exports = config;
